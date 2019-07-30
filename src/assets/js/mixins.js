1e3bbd31789c38b99e43e63b863658333190daeb
liufushihai <liufushihai@163.com>
2019-07-30 10:15:04 +0800

    update: search result link

--- src/assets/js/mixins.js
+++ src/assets/js/mixins.js
@@ -18,10 +18,10 @@ export const searchMixin = {
       storage.set(SEARCH_PAGE_HISTORIES_KEY, words);
 
       //跳转到淘宝搜索
-      location.href = `https://s.m.taobao.com/h5?event_submit_do_new_search_auction=1&_input_charset=utf-8&&topSearch=1&atype=b&searchfrom=1&action=home%3Aredirect_app_action&from=1&sst=1&&n=20&buying=buyitnow&q=${word}`;
+      // location.href = `https://s.m.taobao.com/h5?event_submit_do_new_search_auction=1&_input_charset=utf-8&&topSearch=1&atype=b&searchfrom=1&action=home%3Aredirect_app_action&from=1&sst=1&&n=20&buying=buyitnow&q=${word}`;
 
       //跳转到京东搜索
-      // location.href = `https://so.m.jd.com/ware/search.action?keyword=${word}&searchFrom=home&sf=11&as=1`;
+      location.href = `https://so.m.jd.com/ware/search.action?keyword=${word}&searchFrom=home&sf=11&as=1`;
     }
   }
 };
